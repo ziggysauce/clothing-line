ec30ae29348e1bd42fdf7517bc14dee3c78c832b
Jason Kim <jasonkim3068@gmail.com>
2018-02-08 17:31:30 -0500

    created basic crud routes to create users, created a utils for the has and anti hash, verified the create user route to work via postman, updated the App.js, created a index.js to modularize the routes in the api folder

--- server/app.js
+++ server/app.js
@@ -4,8 +4,20 @@ const bodyparser = require('body-parser');
 
 const app = express();
 const PORT = 5000;
+const cors = require('cors');
+
+require('dotenv').config();
+require('dotenv').load();
+
+const router = require('./api/index');
+
 
 app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json({ limit: '50mb' }));
+
+app.use(cors());
+app.use('/api', router);
+// app.use(bodyparser.urlencoded({ limit: '50mb', extended: true }));
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../app/static/index.html'));

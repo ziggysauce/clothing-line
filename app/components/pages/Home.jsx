42c60768296bb8220feeea3047f5f249682a302c
Jason Kim <jasonkim3068@gmail.com>
2018-02-18 18:52:04 -0500

    created footer

--- app/components/pages/Home.jsx
+++ app/components/pages/Home.jsx
@@ -5,6 +5,7 @@ import { signupUser, loginUser, logoutUser } from '../../actions/login';
 import Navbar from '../navbar/Navbar.jsx';
 import Login from '../user/Login.jsx';
 import Signup from '../user/Signup.jsx';
+import Footer from '../footer/footer.jsx';
 
 class Home extends Component {
   constructor(props) {
@@ -21,6 +22,7 @@ class Home extends Component {
         <button><Link to="/signup">Signup</Link></button>
         <button><Link to="/login">Login</Link></button>
         <button onClick={logoutUser}>Logout</button>
+        <Footer/>
       </div>
     )
   }

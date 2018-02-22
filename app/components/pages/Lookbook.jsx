d2a554715057d49186383aa9c0f623efa9b91e1b
Jason Kim <jasonkim3068@gmail.com>
2018-02-21 23:27:59 -0500

    testing slack integration

--- app/components/pages/Lookbook.jsx
+++ app/components/pages/Lookbook.jsx
@@ -5,7 +5,7 @@ class Lookbook extends Component {
   render() {
     return (
       <div>
-        <h1>This is the Lookbook page</h1>
+        <h1>This is the Lookbook page!</h1>
       </div>
     );
   }
